78642806d5895377a233268e5b05b583c2e43eae
gyc <836732982@qq.com>
2019-08-27 11:06:12 +0800

    客户端代码。。

--- src/main/java/com/masami/nettyDemo/nettyClient.java
+++ src/main/java/com/masami/nettyDemo/nettyClient.java
@@ -2,18 +2,25 @@ package com.masami.nettyDemo;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
-import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: gyc
  * @Date: 2019/8/23 11:50
  */
 public class nettyClient {
+
+    public static int MAX_RETRY = 3;
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -25,13 +32,46 @@ public class nettyClient {
                         channel.pipeline().addLast(new StringEncoder());
                     }
                 });
-        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+        bootstrap.attr(AttributeKey.newInstance("clientName"), "nettyClient");
+        bootstrap.option(ChannelOption.CONNECT_TIMEOUT_MILLIS,5000)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.TCP_NODELAY, true);
+        connect(bootstrap, "127.0.0.1", 8886, 3);
 
-        while (true){
-            channel.writeAndFlush(new Date()+": hello world!");
-            Thread.sleep(2000);
-        }
+
 
 
     }
+
+
+    /**
+     * 重新连接
+     * @param bootstrap
+     * @param ip
+     * @param port
+     * @param retry
+     * @return
+     */
+    public static ChannelFuture  connect(Bootstrap bootstrap , String ip ,int port ,int retry){
+        ChannelFuture channelFuture = bootstrap.connect(ip, port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if (future.isSuccess()) {
+                    System.out.println("连接" + ip + ":" + port + "成功");
+                    return ;
+                }
+                if(retry == 0 ){
+                    System.out.println("连接" + ip + ":" + port + "失败");
+                    return;
+                }
+                int delay = 1;  //失败后，1秒再连
+                bootstrap.config().group().schedule(() -> connect(bootstrap, ip, port, retry - 1), delay, TimeUnit.SECONDS);
+            }
+        });
+
+
+        return channelFuture;
+
+    }
+
+
 }
