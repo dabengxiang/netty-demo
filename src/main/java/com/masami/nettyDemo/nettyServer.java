b8be3bcdb910a8683ac84f8ecc5e0a45819f1461
gyc <836732982@qq.com>
2019-08-27 10:41:22 +0800

    服务端启动代码完成。。

--- src/main/java/com/masami/nettyDemo/nettyServer.java
+++ src/main/java/com/masami/nettyDemo/nettyServer.java
@@ -1,13 +1,13 @@
 package com.masami.nettyDemo;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 /**
  * @Author: gyc
@@ -25,21 +25,46 @@ public class nettyServer {
         NioEventLoopGroup worker = new NioEventLoopGroup(); //worker对应 IOClient.java中的负责读取数据的线程，主要用于读取数据以及业务逻辑处理
 
 
+        final AttributeKey childAttr = AttributeKey.newInstance("childAttr");
         serverBoostrap.group(boss,worker)
                 .channel(NioServerSocketChannel.class)
+                .attr(AttributeKey.newInstance("attr"),"attr")
+                .childAttr(childAttr,"childAttr")
                 .childHandler(new ChannelInitializer() {
                     protected void initChannel(Channel channel) throws Exception {
+                        System.out.println(channel.attr(childAttr).get());
+//                        System.out.println(channel.attr(AttributeKey.newInstance("attr")));
                         channel.pipeline().addLast(new StringDecoder());
                         channel.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
                             protected void channelRead0(ChannelHandlerContext channelHandlerContext, String msg) throws Exception {
                                 System.out.println(msg);
+
                             }
                         });
                     }
-                }).bind(8000);
-
+                }).childOption(ChannelOption.SO_KEEPALIVE,true)
+                .childOption(ChannelOption.TCP_NODELAY,true)
+                .option(ChannelOption.SO_BACKLOG,1024);
+        bind(serverBoostrap,8887);
 
     }
 
+
+    //自动绑定递增端口
+    public static void bind(final ServerBootstrap serverBoostrap , final int port){
+        serverBoostrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if(future.isSuccess()){
+                    System.out.println("绑定端口成功,端口号:" + port);
+                }
+                else{
+                    System.out.println("绑定端口:" + port + "失败");
+                    bind(serverBoostrap,port+1);
+                }
+
+            }
+        });
+
+    }
 
 }
